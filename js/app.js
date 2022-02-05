759d79efa3c18bf7e6d195c1c3e3225be034438e
jhoseph <japoma_200@hotmail.com>
2022-02-04 23:34:45 -0500

    temporizador add

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ import { ageToDays } from "./age-day.js";
 import { alarm, clock } from "./clock.js";
 import { moveBall, resetButton, shortCts, testUp } from "./keyevts.js";
 import hamburgerMenu from "./menu-ham.js";
+import { tempo } from "./tempo.js";
 
 const d = document;
 
@@ -11,6 +12,7 @@ d.addEventListener("DOMContentLoaded", evt => {
   clock(".btn1", ".btn2");
   alarm(".btn3", ".btn4");
   resetButton(".btn_reset", ".circle");
+  tempo(".temporizador");
 });
 
 d.addEventListener("keydown", evt => {
---
+++ js/tempo.js
@@ -0,0 +1,55 @@
+const d = document;
+const $h3 = d.createElement("h3");
+let idInterval;
+
+// const date =  new Date("2022-25-12")
+
+const tempo = tempoClass => {
+  $h3.style.fontSize = "1.5rem";
+  $h3.style.display = "inline-block";
+  $h3.style.padding = ".5rem .7rem";
+  //
+  const $art = d.querySelector(tempoClass);
+  $art.appendChild($h3);
+  coutingDown();
+};
+
+function coutingDown() {
+  idInterval = setInterval(() => {
+    setTime();
+  }, 1000);
+}
+
+function setTime() {
+  const date = new Date(2022, 11, 25).getTime();
+  const dateToday = new Date().getTime();
+  const tmp = Number(date) - Number(dateToday);
+
+  let days = Math.floor(tmp / (24 * 60 * 60 * 1000)),
+    daysms = tmp % (24 * 60 * 60 * 1000),
+    hours = Math.floor(daysms / (60 * 60 * 1000)),
+    hoursms = tmp % (60 * 60 * 1000),
+    minutes = Math.floor(hoursms / (60 * 1000)),
+    minutesms = tmp % (60 * 1000),
+    sec = Math.floor(minutesms / 1000);
+
+  $h3.innerHTML = `faltan: ${days} dias - ${hours} horas - ${minutes} minutos - ${sec} segundos`;
+  stopCounting(sec);
+}
+
+function stopCounting(value) {
+  if (value === 0) {
+    clearInterval(idInterval);
+    idInterval = null;
+    // alerta
+    $h3.remove();
+    const $alert = d.createElement("h1"),
+      $article = d.querySelector(".temporizador");
+    $article.appendChild($alert);
+    $alert.textContent = "FELICITACIONES ES NAVIDAD";
+    $alert.style.color = "rgb(251, 214, 23)";
+    $alert.style.textShadow = "1px 1px 2px red, 0 0 1em yellow, 0 0 0.2em black";
+  }
+}
+
+export { tempo };

cd85c078c2aef146e018b335eeef47a2a9657d2b
jhoseph <japoma_200@hotmail.com>
2022-02-09 20:44:05 -0500

    set localstorage darkMode

--- js/btn-darkmode.js
+++ js/btn-darkmode.js
@@ -2,25 +2,41 @@ const d = document;
 let sun = "☀️";
 let moon = "🌙";
 const darkMode = (btnDark, spanBtn) => {
-  //set attribute to html
-  d.documentElement.setAttribute("data-theme", "light");
+  let theme = localStorage.getItem("data-theme");
+  //
+  let $moonContent = d.querySelector(spanBtn);
+
+  const enableDarkMode = () => {
+    d.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("data-theme", "dark");
+  };
+  const enableLightMode = () => {
+    d.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("data-theme", "light");
+  };
+
+  if (theme === "dark") {
+    enableDarkMode();
+    $moonContent.textContent = sun;
+  }
 
   d.addEventListener("click", evt => {
     if (evt.target.matches(btnDark) || evt.target.matches(`${btnDark} *`)) {
-      // get the current selected theme, should be "light"
-      const currentTheme = d.documentElement.getAttribute("data-theme");
-
-      //swtich beetwen themes
-      const switchTheme = currentTheme === "dark" ? "light" : "dark";
-
-      // change icon buttons
+      // // change icon buttons
       let $moon = d.querySelector(spanBtn).textContent;
       let $moonContent = d.querySelector(spanBtn);
+      // const changeIconButton = $moon === moon ? ($moonContent.textContent = sun) : ($moonContent.textContent = moon);
+      let theme = localStorage.getItem("data-theme");
 
-      const changeIconButton = $moon === moon ? ($moonContent.textContent = sun) : ($moonContent.textContent = moon);
-
-      //  set theme
-      d.documentElement.setAttribute("data-theme", switchTheme);
+      if (theme === "dark") {
+        enableLightMode();
+        // const changeIconButton = $moon === moon ? ($moonContent.textContent = sun) : ($moonContent.textContent = moon);
+        $moonContent.textContent = moon;
+      } else {
+        enableDarkMode();
+        // const changeIconButton = $moon === moon ? ($moonContent.textContent = sun) : ($moonContent.textContent = moon);
+        $moonContent.textContent = sun;
+      }
     }
   });
 };

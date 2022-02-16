7f929d3e46cefe028ab4abdbaba4edf2edd3e06e
jhoseph <japoma_200@hotmail.com>
2022-02-16 16:44:31 -0500

    transition effect added

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,22 +1,26 @@
 const d = document;
 export default function busqueda() {
-  var figcValue;
+  let figcValue;
   d.addEventListener("keyup", evt => {
     if (evt.target.matches("#search_input")) {
       const input = d.querySelector("#search_input");
       const filter = input.value.toUpperCase();
       const $figcap = d.getElementsByTagName("figcaption");
       const $figure = d.getElementsByTagName("figure");
-      console.log($figcap);
-      console.log($figure);
 
       for (let i = 0; i < $figcap.length; i++) {
         figcValue = $figcap[i].textContent || $figcap[i].innerHTML;
         //    search
         if (figcValue.toUpperCase().indexOf(filter) > -1) {
-          $figure[i].style.display = "";
+          $figure[i].style.visibility = "visible";
+          $figure[i].style.opacity = "1";
+          $figure[i].style.order = 0;
+          // $figure[i].style.display = "";
         } else {
-          $figure[i].style.display = "none";
+          // $figure[i].style.display = "none";
+          $figure[i].style.visibility = "hidden";
+          $figure[i].style.opacity = "0";
+          $figure[i].style.order = 1;
         }
       }
     }
